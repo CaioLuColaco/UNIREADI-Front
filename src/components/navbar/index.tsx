0b12f7282bf8c5441ad2b323870df7d9a2b477c7
caio lucena <caio.lcolaco@gmail.com>
2023-04-07 11:45:44 -0300

    created the navbar's component

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,17 @@
+import styles from "./styles/Navbar.module.css"
+
+export default function Navbar() {
+    return (
+        <div className={styles.navContainer}>
+            <div className={styles.logoBox}>
+                <img src="/logoReadi.png" alt="Logo da UniReadi" />
+                <h3>UniReadi</h3>
+            </div>
+
+            <div className={styles.linkPages}>
+                <a href="">Home</a>
+                <a href="">login</a>
+            </div>
+        </div>
+    )
+}
