8b0657775c7c7c50f7af0c5b382577145ee995f0
caio lucena <caio.lcolaco@gmail.com>
2023-04-08 12:26:17 -0300

    register process are working

--- src/pages/coordinator/processDash.tsx
+++ src/pages/coordinator/processDash.tsx
@@ -4,8 +4,80 @@ import Head from "next/head"
 import { parseCookies } from "nookies"
 import styles from '@/styles/CoordinatorDash.module.css'
 import Navbar from "@/components/navbar"
+import { useState } from "react"
+import { api } from "@/services/api"
 
 export default function ProcessDash({ user }: any) {
+
+    const [dataUser, setDataUser] = useState(user)
+
+    const [nameProcess, setNameProcess] = useState("")
+    const [descriptionProcess, setDescriptionProcess] = useState("")
+    const [statusProcess, setStatusProcess] = useState("")
+    const [vacancysProcess, setVacancysProcess] = useState(0)
+    const [scholarShipsProcess, setScholarShipsProcess] = useState(0)
+    const [beginDateProcess, setBeginDateProcess] = useState("")
+    const [endDateProcess, setEndDateProcess] = useState("")
+
+    const [alertRegister, setAlertRegister] = useState("")
+
+    const handleProcessRegister = async () => {
+        if(nameProcess == "") {
+            setAlertRegister("Preencher nome!")
+            return
+        }
+
+        if(vacancysProcess == 0) {
+            setAlertRegister("Preencher número de vagas!")
+            return
+        }
+        
+        if(vacancysProcess < scholarShipsProcess) {
+            setAlertRegister("Número de vagas menor que o número de bolsas!")
+            return
+        }
+        
+        if(beginDateProcess == "") {
+            setAlertRegister("Preencher data inicial!")
+            return
+        }
+
+        if(endDateProcess == "") {
+            setAlertRegister("Preencher data final!")
+            return
+        }
+        
+
+        await api.post('/process', {
+            name: nameProcess,
+            description: descriptionProcess? descriptionProcess : "",
+            status: statusProcess,
+            vacancys: vacancysProcess,
+            scholarships: scholarShipsProcess,
+            beginDate: beginDateProcess,
+            endDate: endDateProcess,
+            course: dataUser.course,
+            creatorId: dataUser.id
+        }).then((res) => {
+
+            setAlertRegister("")
+            setBeginDateProcess("")
+            setDescriptionProcess("")
+            setEndDateProcess("")
+            setNameProcess("")
+            setScholarShipsProcess(0)
+            setStatusProcess("")
+            setVacancysProcess(0)
+
+            updateUser()
+
+        }).catch((error) => {
+            console.log(error)
+        })
+    }
+
+    const updateUser = async () => await api.get(`/user/${user.id}`).then((res) => setDataUser(res.data)).catch((error) => console.log(error))
+
     console.log(user)
     return (
         <>
@@ -26,7 +98,7 @@ export default function ProcessDash({ user }: any) {
 
                         <div className={styles.processList}>
 
-                            {user.createdProcess.map((process: any) => {
+                            {dataUser.createdProcess.map((process: any) => {
                                 return (
                                     <div className={styles.processCard} key={process.id}>
                                         <div className={styles.cardContent}>
@@ -72,45 +144,48 @@ export default function ProcessDash({ user }: any) {
                         <div className={styles.editorProcess}>
                             <div>
                                 <h3 className={styles.editorLabel}>Nome</h3>
-                                <input type="text" placeholder="Escreva aqui..." onChange={(e) => console.log(e)} value={""}/>
+                                <input type="text" placeholder="Escreva aqui..." onChange={(e) => setNameProcess(e.target.value)} value={nameProcess}/>
                             </div>
 
                             <div>
                                 <h3 className={styles.editorLabel}>Descrição</h3>
-                                <input type="text" placeholder="Escreva aqui..." onChange={(e) => console.log(e)} value={""}/>
+                                <input type="text" placeholder="Escreva aqui..." onChange={(e) => setDescriptionProcess(e.target.value)} value={descriptionProcess}/>
                             </div>
 
                             <div className={styles.editorDataNumbers}>
                                 <div>
                                     <h3 className={styles.editorLabel}>Vagas</h3>
-                                    <input type="number" placeholder="Escreva aqui..." onChange={(e) => console.log(e)} value={""}/>
+                                    <input type="number" placeholder="Escreva aqui..." onChange={(e) => setVacancysProcess(parseInt(e.target.value))} value={vacancysProcess}/>
                                 </div>
                                 <div>
                                     <h3 className={styles.editorLabel}>Bolsas</h3>
-                                    <input type="number" placeholder="Escreva aqui..." onChange={(e) => console.log(e)} value={""}/>
+                                    <input type="number" placeholder="Escreva aqui..." onChange={(e) => setScholarShipsProcess(parseInt(e.target.value))} value={scholarShipsProcess}/>
                                 </div>
                             </div>
 
                             <div className={styles.editorDataNumbers}>
                                 <div>
                                     <h3 className={styles.editorLabel}>Inicio</h3>
-                                    <input type="date" placeholder="Escreva aqui..." onChange={(e) => console.log(e)} value={""}/>
+                                    <input type="date" placeholder="Escreva aqui..." min={`${new Date().toISOString().substr(0, 10)}`} max={endDateProcess!=""? `${endDateProcess}` : "2100-04-01"} onChange={(e) => setBeginDateProcess(e.target.value)} value={beginDateProcess}/>
                                 </div>
-                                <div>
-                                    <h3 className={styles.editorLabel}>Fim</h3>
-                                    <input type="date" placeholder="Escreva aqui..." onChange={(e) => console.log(e)} value={""}/>
-                                </div>
+                                {beginDateProcess!="" && 
+                                    <div>
+                                        <h3 className={styles.editorLabel}>Fim</h3>
+                                        <input type="date" placeholder="Escreva aqui..." min={`${beginDateProcess}`} onChange={(e) => setEndDateProcess(e.target.value)} value={endDateProcess}/>
+                                    </div>
+                                }
                             </div>
 
                             <div>
                                 <h3 className={styles.editorLabel}>Status</h3>
-                                <select name="status" onChange={(e) => console.log(e.target.value)} value={""}>
+                                <select name="status" onChange={(e) => setStatusProcess(e.target.value)} value={statusProcess}>
                                     <option value="aberto">Aberto</option>
                                     <option value="fechado">Fechado</option>
                                 </select>
                             </div>
 
-                            <button onClick={() => console.log()}>Cadastrar</button>
+                            <button onClick={() => handleProcessRegister()}>Cadastrar</button>
+                            <p className={styles.alertRegister}>{alertRegister}</p>
                         </div>
                     </div>
                 </div>
