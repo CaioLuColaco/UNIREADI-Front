8f90337b959bc38976c45e735a8b375e0c59af5a
caio lucena <caio.lcolaco@gmail.com>
2023-04-08 01:29:15 -0300

    test the auth with dash

--- src/pages/student/processDash.tsx
+++ src/pages/student/processDash.tsx
@@ -1,3 +1,32 @@
+import { AuthContext } from "@/contexts/AuthContext"
+import { GetServerSideProps } from "next"
+import { parseCookies } from "nookies"
+import { useContext } from "react"
+
 export default function ProcessDash() {
-    return <h3>ProcessDash Student</h3>
+    const { user } = useContext(AuthContext)
+    console.log(user)
+    return (
+        <>
+            <h3>ProcessDash Student</h3>
+            <h2>Olá {user?.name}</h2>
+        </> 
+    )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    const { ['uniReadiToken']: token} = parseCookies(ctx)
+
+    if(!token) {
+        return {
+            redirect: {
+                destination: "/login",
+                permanent: false
+            }
+        }
+    }
+
+    return {
+        props: {}
+    }
 }
