ba140befa2e9f52a8c1798c9ab9433744d6c1131
caio lucena <caio.lcolaco@gmail.com>
2023-04-08 01:28:43 -0300

    configure the api to use the token on cookies

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,14 @@
 import axios from "axios";
+import { parseCookies } from 'nookies'
+
+const { 'uniReadiToken': token } = parseCookies()
 
 const api = axios.create({
     baseURL: process.env.API_URL
 })
+
+if(token) {
+    api.defaults.headers['Authorization'] = `Bearer ${token}`
+}
 
 export default api;
